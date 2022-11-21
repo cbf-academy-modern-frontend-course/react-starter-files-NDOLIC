57095992650041ef7a7e57d9ff84383d0c35b752
tmclaire <tblaguese1@gmail.com>
2022-11-21 21:39:12 +0200

    homework done

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,14 +1,22 @@
+import React,{ useState } from "react";
+import Book from "./components/Book";
+import data from "./models/books.json";
 import React from "react";
 import BookList from "./components/BookList";
 import "./index.css";
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1 className="title">Bookcase App</h1>
-      <BookList />
-    </div>
-  );
-};
+const App = (props) => {
+  const [books] = useState(data);
+
+  return books.map(book => <Book key={book.id} book={book}/>);
+}
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <h1 className="title">Bookcase App</h1>
+//       <BookList />
+//     </div>
+//   );
+// };
 
 export default App;
