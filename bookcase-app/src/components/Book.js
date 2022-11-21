38a971211260ba4abe2de11c1fdcef69f73dbcc9
tmclaire <tblaguese1@gmail.com>
2022-11-21 20:55:41 +0200

    bookcase first code

--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React from 'react'
 
-
-
-//const [books] = useState(data);
-const Book = (props) => {
+const Book = ({ book }) => {
+  const {
+    volumeInfo: {
+      title,
+      authors,
+      description,
+      imageLinks: { thumbnail },
+    },
+    saleInfo: { retailPrice },
+  } = book
+  const handleClick = (title) => {
+    console.log(`The book ${title} was clicked`)
+  }
   return (
     <div>
-      {
-        <h2> {props.book.volumeInfo.title}</h2>
-      }
-
+      <h1>{title}</h1>
+      {authors.length > 1 ? <p>{authors.join(' & ')}</p> : <p>{authors}</p>}
+      {retailPrice ? (
+        <p>£{retailPrice && retailPrice.amount}</p>
+      ) : (
+        <p>No price found</p>
+      )}
+      <p>{description.substring(0, 400)}...</p>
+      <img src={thumbnail} alt={title} />
+      <button onClick={() => handleClick(title)}>Add +</button>
     </div>
-  );
+  )
 }
+Book.defaultProps = {
+  saleInfo: {
+    retailPrice: {
+      amount: 'No price found',
+    },
+  },
+}
+
 export default Book
 
