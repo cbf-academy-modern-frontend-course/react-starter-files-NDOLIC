bf380a785c3682b011bf0e6d344abb438a4ea575
tmclaire <tblaguese1@gmail.com>
2022-11-21 22:00:14 +0200

    task done, homework not yet done

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import TaskContainer from './components/TaskContainer';
-/*
+import Task from './components/Task';
+
 const taskList = [
   {"id":"task1","title":"Buy Milk","isComplete":true},
   {"id":"task2","title":"Read a book","isComplete":false},
@@ -10,15 +11,31 @@ const taskList = [
   {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
   {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
 ];
-*/
+
+const getSortedElements = (sortByBool) => {
+  return taskList.filter((task) => task.isComplete == sortByBool);
+}
+
+function toggleComplete(id){
+  console.log(`Task with the id '${id}' was clicked`);
+}
 
 function App() {
-  
+  let taskList = getSortedElements(false).concat(getSortedElements(true));
    return (
       <Fragment>
         <h1 key="heading">Task List</h1>
         <TaskContainer>
           {/* Task: Nesting Components - Add you tasks here: */}
+          {taskList.map((task) => (<Task 
+             key={task.id}
+             title={task.title}
+             isComplete={task.isComplete}
+             toggleComplete={toggleComplete}
+             id={task.id} />
+          
+          ))}
+          
         </TaskContainer>
       </Fragment>
   );
